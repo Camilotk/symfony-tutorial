49199b4526a42477de86bb68a096a52c413c45ae
Camilo Cunha de Azevedo <camilotk@gmail.com>
2020-03-14 02:54:35 -0300

    Add custom method

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -16,5 +18,14 @@ class MainController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MainController.php',
         ]);
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     */
+    public function custom(Request $request)
+    {
+        $name = $request->get('name');
+        return new Response('<h1>Olá ' . $name. '!</h1>');
     }
 }
