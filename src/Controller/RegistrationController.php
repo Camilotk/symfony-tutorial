add9b50f16728534aefb24e7172bb78793db8707
Camilo Cunha de Azevedo <camilotk@gmail.com>
2020-03-21 15:20:10 -0300

    finished the auth part

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,18 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/registration", name="registration")
+     * @Route("/register", name="register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
      */
-    public function index()
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
+        $form = $this->createFormBuilder()
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password']
+            ])
+            ->add('save', SubmitType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            // pega os dados do formulário
+            $data = $form->getData();
+
+            // cria o objeto User a ser persistido
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setPassword($encoder->encodePassword($user, $data['password']));
+
+            // persist the data
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('app_login'));
+        }
         return $this->render('registration/index.html.twig', [
             'controller_name' => 'RegistrationController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -94,8 +94,7 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('post'));
     }
 
     protected function getLoginUrl()
